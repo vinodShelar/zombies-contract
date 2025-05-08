d65ac0e514c03faf6e3d09ece1ab5a0a1a1ac88e
vinodShelar <vinodshelar7757@gmail.com>
2025-05-08 23:56:02 +0400

    changes for understanding new conept

--- contracts/Address.sol
+++ contracts/Address.sol
@@ -2,13 +2,39 @@
 pragma solidity ^0.8.0;
 
 contract AddressExample {
-    address public owner; 
-
-    constructor() {
-        owner = msg.sender;  
+    address public owner;
+    constructor(){
+        owner = msg.sender;
     }
 
-    function getOwner() public view returns (address) {
-        return owner;
+    // Event for tracking deposits
+    event Deposited(address indexed  sender, uint amount);
+    // Event for tracking withdrawans
+    event Withdrawn(address indexed receiver, uint amount);
+    
+    // Function to receive Ether
+    receive() external payable { 
+        emit  Deposited(msg.sender, msg.value);
+    }
+
+    // get balance of this acc
+    function getBalance() public view returns (uint) {
+         return address(this).balance;
+    }
+
+    // withdraw all funds to the owner add
+    function withdraw() public {
+        require(msg.sender == owner, "Caller is not owner");
+        uint balance = address(this).balance;
+        payable(owner).transfer(address(this).balance);
+        emit  Withdrawn(owner, balance);
+    }
+
+    // send ether to user defined address
+    function sendTo(address payable _to, uint _amount) public {
+       require(msg.sender == owner, "Only owner can send");
+       require(address(this).balance >= _amount, "Insufficient balance");
+         _to.transfer(_amount);
+    emit Withdrawn(_to, _amount);
     }
 }
--- contracts/Caculator.sol
+++ contracts/Caculator.sol
@@ -1,23 +1,51 @@
-
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+library SafeMath {
+    function multiply(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) {
+            return 0;
+        }
+        uint256 c = a * b;
+        require(c / a == b, "Multiplication overflow");
+        return c;
+    }
+
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c >= a, "Addition overflow");
+        return c;
+    }
+
+    function subtract(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b <= a, "Subtraction underflow");
+        return a - b;
+    }
+}
+
 contract Calculator {
-    uint256 result=0;
+    uint32 public result = 0;
 
-    function add(uint256 num) public  {
-        result +=num;
+    using SafeMath for uint256;
+
+    function add(uint32 num) public {
+        uint256 tempResult = SafeMath.add(uint256(result), uint256(num));
+        require(tempResult <= type(uint32).max, "Addition overflow in Calculator");
+        result = uint32(tempResult);
     }
 
-    function substract(uint256 num) public  {
-        result -=num;
+    function subtract(uint32 num) public {
+        uint256 tempResult = SafeMath.subtract(uint256(result), uint256(num));
+        result = uint32(tempResult);
     }
 
-    function multiply(uint256 num) public  {
-        result  *= num;
+    function multiply() public {
+        uint256 tempResult = SafeMath.multiply(uint256(result), 4801);
+        require(tempResult <= type(uint32).max, "Multiplication overflow in Calculator");
+        result = uint32(tempResult);
     }
 
-    function get() public view returns (uint256) {
+    function getResult() public view returns (uint32) {
         return result;
     }
 }
